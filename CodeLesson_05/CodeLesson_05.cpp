d8622f32fb931ee63434294f32ee7cdb7594d24d
Aghriamar <A1exXx88@inbox.ru>
2024-05-05 22:51:54 +0300

    Added homework_05

--- CodeLesson_05/CodeLesson_05.cpp
+++ CodeLesson_05/CodeLesson_05.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int N = 5;
+
+int main() {
+    int arr[N][N];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            arr[i][j] = i + j;
+        }
+    }
+
+    std::cout << "Array:" << std::endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    int row = std::time(nullptr) % N;
+    int sum = 0;
+    for (int j = 0; j < N; ++j) {
+        sum += arr[row][j];
+    }
+    std::cout << "Sum of elements in row " << row << ": " << sum << std::endl;
+
+    return 0;
 }
